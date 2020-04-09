f7b4806ad6b8a3a9669c776592905f104e47a3a7
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-04-10 01:22:12 +0530

    code-snippet

---
+++ codesnippet.cpp
@@ -0,0 +1,11 @@
+//shuru apni marzi se kiye the ab fhodne ka man kar raha hai
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+#define sq(a) (a)*(a)
+#define endl "\n"
+#define boost ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+int main(){
+    return 0;
+}
