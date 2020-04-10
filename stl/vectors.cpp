e2794110ca4d964e16b503ef23951e3b8a19f855
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-04-10 14:54:38 +0530

    vectors

---
+++ stl/vectors.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Mrigank Anand on 10/04/20.
+//
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of terms you want in your vector: ";
+    cin>>n;
+    vector<int> a;
+    for(int i=0;i<n;i++)
+    {
+        int b;
+        cin>>b;
+        a.push_back(b);
+
+    }
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";       //printing the elements of vector using direct accessing the index
+    cout<<endl;
+    for(auto i=a.begin();i!=a.end();++i)
+        cout<<*i<<"\n";              //printing the elements of vector using iterators
+    cout<<endl;
+
+    cout<<*(a.data());   // it prints the first element of the vector
+    cout<<endl;
+}
