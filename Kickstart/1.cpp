d92bac0d106cb99f49a8ae7cd43049edf6346402
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-04-19 14:42:24 +0530

    kickstart-round-b-Bike-tour

--- Kickstart/1.cpp
+++ Kickstart/1.cpp
@@ -4,31 +4,33 @@
 
 //shuru apni marzi se kiye the ab fhodne ka man kar raha hai
 #include<iostream>
+
 #define ll long long
 #define sq(a) (a)*(a)
 #define endl "\n"
 #define boost ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 using namespace std;
-int main(){
+
+int main() {
     //Hello world
     ll t;
-    cin>>t;
-    for(ll j=0;j<t;j++){
+    cin >> t;
+    for (ll j = 0; j < t; j++) {
         ll n;
-        cin>>n;
+        cin >> n;
         ll h[n];
-        for(ll i = 0; i < n; i++){
-            cin>>h[i];
+        for (ll i = 0; i < n; i++) {
+            cin >> h[i];
         }
         ll count = 0;
 
-        for(ll i = 1;i < n-1; i++){
-            if(h[i] > h[i-1] && h[i] > h[i+1]){
+        for (ll i = 1; i < n - 1; i++) {
+            if (h[i] > h[i - 1] && h[i] > h[i + 1]) {
                 count++;
             }
         }
 
-        cout<<"Case #"<<j+1<<": "<<count<<"\n";
+        cout << "Case #" << j + 1 << ": " << count << "\n";
 
     }
     return 0;
