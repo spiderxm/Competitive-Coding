9dc3af92d21eb99d5ef653cc42e884840517ed14
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-04-20 22:45:20 +0530

    cookoff

--- codechef/codechef-cookoff-2.cpp
+++ codechef/codechef-cookoff-2.cpp
@@ -1,8 +1,5 @@
+#include<bits/stdc++.h>
 #define ll long long
-
-#include <iostream>
-#include <vector>
-
 #define sq(a) (a*a)
 #define mp make_pair
 #define tp tuple
@@ -16,15 +13,28 @@
 #define endl "\n"
 #define fastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 using namespace std;
-
-ll power(ll a, ll b) {
-    if (b == 0) return 1;
-    ll p = power(a, b / 2) % mod;
-    if (b & 1) return (a * sq(p)) % mod; else return sq(p) % mod;
-}
-
 void test_case();
+ll power(ll x,ll y)
+{
+    ll res = 1;     // Initialize result
 
+    x = x % mod; // Update x if it is more than or
+    // equal to p
+
+    if (x == 0) return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % mod;
+
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % mod;
+    }
+    return res;
+}
 int main()
 {
     fastIO;
