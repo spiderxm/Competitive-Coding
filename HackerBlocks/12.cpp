33e3a6c938dc8f5549e2c29c5b479061f5a61cc4
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-04-30 14:45:18 +0530

    cp

--- HackerBlocks/12.cpp
+++ HackerBlocks/12.cpp
@@ -2,3 +2,48 @@
 // Created by Mrigank Anand on 30/04/20.
 //
 
+//shuru apni marzi se kiye the ab fhodne ka man kar raha hai
+#include<iostream>
+#include<vector>
+
+#define ll long long
+
+using namespace std;
+
+bool compare(string a, string b) {
+    string c = a.append(b);
+    string d = a.append(d);
+    return c.compare(d) > 0 ? 1 : 0;
+}
+
+int myCompare(string X, string Y) {
+    // first append Y at the end of X
+    string XY = X.append(Y);
+
+    // then append X at the end of Y
+    string YX = Y.append(X);
+
+    // Now see which of the two formed numbers is greater
+    return YX.compare(XY) < 0 ? 1 : 0;
+}
+
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        vector<string> a;
+        for (int i = 0; i < n; i++) {
+            string b;
+            cin >> b;
+            a.push_back(b);
+        }
+        sort(a.begin(), a.end(), myCompare);
+        for (int j = 0; j < n; ++j) {
+            cout << a[j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
--- codesnippet.cpp
+++ codesnippet.cpp
@@ -1,14 +1,18 @@
 //shuru apni marzi se kiye the ab fhodne ka man kar raha hai
 #include<iostream>
 #include<vector>
+
 #define ll long long
 #define sq(a) (a)*(a)
 #define endl "\n"
 #define pb push_back
 #define v vector
 #define mp make_pair
+
 #include <algorithm>
+
 #define boost ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
 ll pow(ll a, ll b) {
     ll ans = 1;
     while (b > 0) {
@@ -20,7 +24,9 @@ ll pow(ll a, ll b) {
     }
     return ans;
 }
+
 using namespace std;
-int main(){
+
+int main() {
     return 0;
 }
